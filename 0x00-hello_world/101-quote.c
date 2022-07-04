fc4a3354ad596b795fadbbe307ad243d54f80fd4
Vicpsalms <uchechukwuvic2116@gmail.com>
2022-07-04 08:51:25 -0700

    Hot Fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - prints exactly "and that piece of art is useful""
- * - Dora Korpar, 2015-10-19",
- * followed by a new line, to the standar error.
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19"'
+ * followed by a new line, to the standard error.
  * Return: Always 0 (success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpa, 20215-10-19\n", 59);
+return (1);
 }
