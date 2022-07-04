00527315b1f8530150264011c13784e65d81d16f
Vicpsalms <uchechukwuvic2116@gmail.com>
2022-07-03 17:42:00 -0700

    another correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - prints the phrase "programmingis like building multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
+puts("\"programming is like building a multilingual puzzle")
 return (0);
 }
