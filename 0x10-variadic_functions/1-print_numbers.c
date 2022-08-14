23db13cc659d0ec2e1f84ebb2f428235b66466ed
Vicpsalms <uchechukwuvic2116@gmail.com>
2022-08-14 07:10:44 -0700

    C-VARIADIC AND FUNCTIONS

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,10 +2,10 @@
 
 /**
  * print_numbers - prints numbers given as parameters
- * @seperator: string to be printed between numbers
+ * @separator: string to be printed between numbers
  * @n: number of integers passed to the function
  */
-void print_numbers(const char *seperator, const unsigned int n, ..)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 unsigned int i;
 va_list list;
@@ -14,12 +14,12 @@ va_start(list, n);
 
 for (i = 0; i < n; i++)
 {
-if (!seperator)
+if (!separator)
 printf("%d", va_arg(list, int));
-else if (seperator && i == 0)
+else if (separator && i == 0)
 printf("%d", va_arg(list, int));
 else
-printf("%s%d", seperator, va_arg(list, int));
+printf("%s%d", separator, va_arg(list, int));
 }
 
 va_end(list);
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,10 +2,10 @@
 
 /**
  * print_strings - prints strings
- * @seperator: seperator between strings
+ * @separator: separator between strings
  * @n: number of arguments
  */
-void print_strings(const char *seperator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 unsigned int i;
 char *str;
@@ -19,12 +19,12 @@ for (i - 0; i < n; i++)
 str = va_arg(list, char *);
 if (!str)
 str = "(nil)";
-if (!seperator)
+if (!separator)
 printf("%s", str);
-else if (seperator && i == 0)
+else if (separator && i == 0)
 printf("%s", str);
 else
-printf("%s%s", seperator, str);
+printf("%s%s", separator, str);
 }
 
 printf("\n");
